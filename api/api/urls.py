00344bd7b6002b47adcdc5134466c13357931451
lucasronan33 <164685834+lucasronan33@users.noreply.github.com>
2025-12-04 20:35:59 -0300

    Adiciona modelo Category e relaciona com Product; atualiza serializers e rotas

--- api/api/urls.py
+++ api/api/urls.py
@@ -1,10 +1,11 @@
 from django.contrib import admin
 from django.urls import path, include
 from rest_framework.routers import DefaultRouter
-from products.views import ProductViewSet
+from products.views import ProductViewSet, CategoryViewSet
 
 router = DefaultRouter()
 router.register(r"products", ProductViewSet, basename="products")
+router.register(r"categories", CategoryViewSet, basename="categories")
 
 urlpatterns = [
     path("admin/", admin.site.urls),
--- api/products/serializers.py
+++ api/products/serializers.py
@@ -1,8 +1,23 @@
 from rest_framework import serializers
-from .models import Product
+from .models import Product, Category
+
+
+class CategorySerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Category
+        fields = "__all__"
 
 
 class ProductSerializer(serializers.ModelSerializer):
+    category_name = serializers.CharField(source="category.name", read_only=True)
+
     class Meta:
         model = Product
-        fields = "__all__"
+        fields = [
+            "id",
+            "name",
+            "price",
+            "in_stock",
+            "category",
+            "category_name",
+        ]
